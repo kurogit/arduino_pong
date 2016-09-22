b5f8ca6cc1deea8b0ff041c3c79aa3b3ce243c9a
Patrick Schwartz <kurogit@schwartzm.com>
2016-09-22 17:57:39 +0200

    Add implementation of a basic game loop.
    Derived from http://gameprogrammingpatterns.com/game-loop.html

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,13 +1,36 @@
 #include "Game.hpp"
 
+#include <Arduino.h>
+
+namespace
+{
+
+constexpr uint32_t MsPerUpdate = 16;
+
+}  // namespace
+
 namespace arduino_pong
 {
 
 void Game::run()
 {
+
+    uint32_t previous = millis();
+    uint32_t lag = 0;
+
     while(true)
     {
-        gameField_.update();
+        uint32_t current = millis();
+        uint32_t elapsed = current - previous;
+        previous = current;
+        lag += elapsed;
+
+        while(lag >= MsPerUpdate)
+        {
+            gameField_.update();
+			lag -= MsPerUpdate;
+        }
+
         gameField_.render();
     }
 }
