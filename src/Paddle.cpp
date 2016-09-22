16dcf6ed2850558f7cbba37cd2686502765d983b
Patrick Schwartz <kurogit@schwartzm.com>
2016-09-22 18:32:38 +0200

    Turn Paddle into its own class

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -1,1 +1,16 @@
 #include "Paddle.hpp"
+
+namespace arduino_pong
+{
+
+Paddle::Paddle(int x, int y)
+    : bounds_{x, y, Paddle::Width, Paddle::Height}
+{
+}
+
+const Rectangle& Paddle::bounds() const
+{
+    return bounds_;
+}
+
+}  // namespace arduino_pong
--- src/Paddle.hpp
+++ src/Paddle.hpp
@@ -5,6 +5,19 @@
 namespace arduino_pong
 {
 
-using Paddle = Rectangle;
+class Paddle
+{
+public:
+    static constexpr int Width = 6;
+    static constexpr int Height = 20;
+
+public:
+    Paddle(int x, int y);
+
+    const Rectangle& bounds() const;
+
+private:
+    Rectangle bounds_;
+};
 
 }  // namespace arduino_pong
