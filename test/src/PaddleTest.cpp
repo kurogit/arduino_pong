d26ebfd97b7ed856b426ec2ddabf044d7fea6115
Patrick Schwartz <kurogit@schwartzm.com>
2016-09-23 18:24:50 +0200

    Add test for Paddle

---
+++ test/src/PaddleTest.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+#include <Paddle.hpp>
+#include <Rectangle.hpp>
+
+TEST_CASE("Constructor and getters")
+{
+	using arduino_pong::Paddle;
+
+	Paddle paddle{1, 2};
+
+	const auto& bounds = paddle.bounds();
+	CHECK(bounds.x() == 1);
+	CHECK(bounds.y() == 2);
+	CHECK(bounds.width() == 6);
+	CHECK(bounds.height() == 20);
+}
