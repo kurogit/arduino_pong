db27e360b3a816b9a63b3ee0ffecf0027dd068f4
Patrick Schwartz <kurogit@schwartzm.com>
2016-09-23 18:25:53 +0200

    Add first logic to GameField

--- src/GameField.cpp
+++ src/GameField.cpp
@@ -1,7 +1,27 @@
 #include "GameField.hpp"
 
+#include "Renderer.hpp"
+
+namespace
+{
+
+constexpr int FieldWidth = 160;
+constexpr int FieldHeigt = 128;
+constexpr int FieldCenter[] = {FieldWidth / 2, FieldHeigt / 2};
+
+constexpr int LeftPaddleXPosition = 2;
+constexpr int RightPaddleXPosition = FieldWidth - arduino_pong::Paddle::Width - 2;
+
+}  // namespace
+
 namespace arduino_pong
 {
+
+GameField::GameField()
+    : ball_{FieldCenter[0], FieldCenter[1]},
+      paddles_{{LeftPaddleXPosition, FieldCenter[1]}, {RightPaddleXPosition, FieldCenter[1]}}
+{
+}
 
 void GameField::update()
 {
@@ -9,7 +29,11 @@ void GameField::update()
 
 void GameField::render(Renderer& renderer)
 {
-	(void)renderer;
+    renderer.renderFieldLine();
+
+    renderer.render(ball_.bounds());
+    renderer.render(paddles_[0].bounds());
+    renderer.render(paddles_[1].bounds());
 }
 
 }  // namespace arduino_pong
--- src/GameField.hpp
+++ src/GameField.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Ball.hpp"
+#include "Paddle.hpp"
+
 namespace arduino_pong
 {
 
@@ -8,8 +11,13 @@ class Renderer;
 class GameField
 {
 public:
+	GameField();
+
     void update();
     void render(Renderer& renderer);
+private:
+	Ball ball_;
+	Paddle paddles_[2];
 };
 
 }  // namespace arduino_pong
