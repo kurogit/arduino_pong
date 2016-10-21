b3da7339409400f0a8693f77fe6a79deeacf0959
Patrick Schwartz <kurogit@schwartzm.com>
2016-10-21 18:08:31 +0200

    Add license information

--- src/CollisionProcessor.hpp
+++ src/CollisionProcessor.hpp
@@ -1,3 +1,8 @@
+/*!
+ * \file
+ * \details This file is part of https://github.com/kurogit/arduino_pong which is licensed under the MIT License.
+ * \copyright 2016 Patrick Schwartz <kurogit@schwartzm.com>
+ */
 #pragma once
 
 namespace arduino_pong
